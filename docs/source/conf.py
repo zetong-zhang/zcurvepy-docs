f71bd937c427b9f69b04d64f6265dc93d6cc28d9
zetong-zhang <flypigaround@163.com>
2025-03-10 16:12:58 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ZCurvePy'
+copyright = '2025, TUBIC'
+author = 'TUBIC'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.5'
+version = '1.5.7'
 
 # -- General configuration
 
