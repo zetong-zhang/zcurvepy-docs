b4564ba5fd54c6a56458095aed999fe7acab0612
zetong-zhang <flypigaround@163.com>
2025-03-14 17:26:47 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,12 +44,6 @@ myst_linkify_enabled = True
 
 templates_path = ['_templates']
 
-html_static_path = ["_static"]
-
-html_css_files = [
-    "css/custom.css",
-]
-
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
